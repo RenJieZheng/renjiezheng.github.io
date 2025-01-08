29f45f34e6004a511e945fe18569d4b75d7b0324
RenJieZheng <42659888+RenJieZheng@users.noreply.github.com>
2025-01-08 13:44:22 -0500

    projects - ray tracer details

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,17 +1,13 @@
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
-// Check for saved theme in localStorage
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme) {
     body.classList.add(savedTheme);
 }
 
-// Toggle dark mode
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
-
-    // Save the current theme in localStorage
     const currentTheme = body.classList.contains('dark-mode') ? 'dark-mode' : '';
     localStorage.setItem('theme', currentTheme);
 });
